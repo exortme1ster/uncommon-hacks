8cf19d00eb95b0d9a9e0d7ef33504da9083dc0e2
Nikita Mashchenko <52038455+exortme1ster@users.noreply.github.com>
2023-03-25 15:59:12 -0500

    added chatgpt support

---
+++ functionality/chatgpt.ts
@@ -0,0 +1,7 @@
+import { Configuration, OpenAIApi } from "openai";
+
+export const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: "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",
+  })
+);
