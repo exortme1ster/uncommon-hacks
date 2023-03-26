967e6f3324fe5a504112d4e6af116943393172b4
dserra22 <75829857+dserra22@users.noreply.github.com>
2023-03-26 14:16:22 -0500

    ok
    ok

--- functionality/chatgpt.ts
+++ functionality/chatgpt.ts
@@ -2,6 +2,6 @@ import { Configuration, OpenAIApi } from "openai";
 
 export const openai = new OpenAIApi(
   new Configuration({
-    apiKey: "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",
+    apiKey: "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",
   })
 );
